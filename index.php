230c2b07eef7a182863aaa35f6c5628ec51a1f0b
Sejal Sali <sejalsali070402@gmail.com>
2023-03-09 20:58:00 +0000

    changed html files to php files

--- index.php
+++ index.php
@@ -43,7 +43,7 @@
 
 <main class="container">
   <div class="starter-template text-center">
-    <h1>Bootstrap starter template</h1>
+    <h1>Hello Sejal</h1>
     <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
   </div>
 
